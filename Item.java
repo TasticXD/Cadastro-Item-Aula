03162608ff64b2762663f1378350dd75f5fae648
TasticXD <162758302+TasticXD@users.noreply.github.com>
2024-04-26 15:18:45 -0300

    Item.java

---
+++ Item.java
@@ -0,0 +1,30 @@
+package produtos;
+
+public class Item {
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public Item(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Item: " + nome + " Quantidade: " + quantidade + " Preço (R$): " + String.format("%.2f", preco);
+    }
+}
